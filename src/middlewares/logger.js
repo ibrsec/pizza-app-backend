e9fdcb8deadc73136a18b6f153392cfc83aef2aa
ibr <ibr.seckin@gmail.com>
2024-07-31 12:14:12 +0300

    log

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -6,11 +6,12 @@
 // app.use(logger):
 
 const morgan = require('morgan')
-const fs = require('node:fs')
+// const fs = require('node:fs')
 
-const now = new Date()
-const today = now.toISOString().split('T')[0]
+// const now = new Date()
+// const today = now.toISOString().split('T')[0]
 
-module.exports = morgan('combined', {
-    stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
-})
+// module.exports = morgan('combined', {
+//     stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
+// })
+module.exports = morgan('combined');
